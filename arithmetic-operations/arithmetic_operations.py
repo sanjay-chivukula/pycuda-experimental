e0330e09aa41f18771eaccf54ea8b9bd8dc41fa9
Sanjay Chivukula <sanjay.chivukula@gmail.com>
2020-06-03 11:52:19 -0400

    Add arithmetic-operations/ subproject.

---
+++ arithmetic-operations/arithmetic_operations.py
@@ -0,0 +1,36 @@
+import pycuda.autoinit
+import pycuda.driver as cuda
+from pycuda import gpuarray
+import numpy as np
+
+# Revising old concepts.
+gpu_device_count = cuda.Device.count()
+print(f"Device Count: {gpu_device_count}")
+
+try:
+    gpu_device = cuda.Device(0)
+    print(f"Device Name: {gpu_device.name()}")
+except cuda.LogicError as l_error:
+    print("Check the ordinal used to access the cuda device.")
+    raise l_error
+
+# host data.
+# remember, it is better to explicitly set numpy array dtype.
+x_host = np.array(
+    [1, 2, 3],
+    dtype=np.float32  # Equivalent to C_type: float.
+)
+
+y_host = np.array(
+    [2, 3, 5],
+    dtype=np.float64  # Equivalent to C_type: double.
+)
+
+
+# device data.
+x_device = gpuarray.to_gpu(x_host)
+y_device = gpuarray.to_gpu(y_host)
+result_device = x_device ** y_device
+result_host = result_device.get()
+
+print(result_host, result_host.dtype)
