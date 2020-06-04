4532c289ce566395eb027344f4ac3a3e255c2132
Sanjay Chivukula <sanjay.chivukula@gmail.com>
2020-06-03 20:34:27 -0400

    Add code for speed_test.py.

---
+++ speed-test/speed_test.py
@@ -0,0 +1,29 @@
+"""
+This script is written to compare the speed of computation of an operation
+between host and device.
+"""
+from time import time
+import numpy as np
+import pycuda.autoinit
+from pycuda import gpuarray
+
+DATA_SIZE = 10000000
+host_data = np.float32(np.random.random(DATA_SIZE))
+
+# scalar multiplication with 2 on host machine.
+start_time = time()
+result_host_operation = host_data * np.float32(2)
+end_time = time()
+run_time_hostop = end_time - start_time
+
+# scalar multiplication with 2 on device machine.
+device_data = gpuarray.to_gpu(host_data)
+
+start_time = time()
+result_device_operation = device_data * np.float32(2)
+end_time = time()
+run_time_deviceop = end_time - start_time
+
+# runtime display for comparison.
+print(f"Host machine took: {run_time_hostop:.4f} seconds")
+print(f"Device machine took: {run_time_deviceop: .4f} seconds")
