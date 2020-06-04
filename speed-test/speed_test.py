afcd2af7ef892cfd5f11d5984213c08ac54f61fe
Sanjay Chivukula <sanjay.chivukula@gmail.com>
2020-06-03 20:49:43 -0400

    Update speed_test.py: Runs comparison in loops to reflect initial compile using nvcc.

--- speed-test/speed_test.py
+++ speed-test/speed_test.py
@@ -10,20 +10,23 @@ from pycuda import gpuarray
 DATA_SIZE = 10000000
 host_data = np.float32(np.random.random(DATA_SIZE))
 
-# scalar multiplication with 2 on host machine.
-start_time = time()
-result_host_operation = host_data * np.float32(2)
-end_time = time()
-run_time_hostop = end_time - start_time
+for run_number in range(10):
+    # scalar multiplication with 2 on host machine.
+    start_time = time()
+    result_host_operation = host_data * np.float32(2)
+    end_time = time()
+    run_time_hostop = end_time - start_time
 
-# scalar multiplication with 2 on device machine.
-device_data = gpuarray.to_gpu(host_data)
+    # scalar multiplication with 2 on device machine.
+    device_data = gpuarray.to_gpu(host_data)
 
-start_time = time()
-result_device_operation = device_data * np.float32(2)
-end_time = time()
-run_time_deviceop = end_time - start_time
+    start_time = time()
+    result_device_operation = device_data * np.float32(2)
+    end_time = time()
+    run_time_deviceop = end_time - start_time
 
-# runtime display for comparison.
-print(f"Host machine took: {run_time_hostop:.4f} seconds")
-print(f"Device machine took: {run_time_deviceop: .4f} seconds")
+    # runtime display for comparison.
+    print(f"For run number: {run_number}")
+    print(f"Host machine took: {run_time_hostop:.4f} seconds")
+    print(f"Device machine took: {run_time_deviceop: .4f} seconds")
+
